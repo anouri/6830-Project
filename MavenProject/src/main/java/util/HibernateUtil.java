9662d169a372fdb14ad07e5a3ec66b35d94da2dc
shirllu <shirley.lu100@gmail.com>
2014-11-14 08:54:37 -0500

    Semi working hibernate for infinispan.

--- MavenProject/src/main/java/util/HibernateUtil.java
+++ MavenProject/src/main/java/util/HibernateUtil.java
@@ -3,16 +3,14 @@ package util;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
   
     private static SessionFactory sessionFactory;
-    
-    public static SessionFactory createSessionFactory() {
-        Configuration configuration = new Configuration().configure("/resources/hibernate.cfg.xml");
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-        sessionFactory = configuration.buildSessionFactory(builder.build());
+    public static SessionFactory createSessionFactory(Configuration cfg) {
+        //Configuration configuration = new Configuration().configure("/resources/hibernate.cfg.xml");
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
+        sessionFactory = cfg.buildSessionFactory(builder.build());
         return sessionFactory;
     }
     
--- MavenProject/src/test/java/HibernateDemo.java
+++ MavenProject/src/test/java/HibernateDemo.java
@@ -1,28 +1,90 @@
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+
 import util.*;
 import datamodel.*;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.ogm.cfg.OgmConfiguration;
 import org.hibernate.*;
+import org.jboss.jandex.Main;
+
+import com.arjuna.ats.jta.TransactionManager;
 
 public class HibernateDemo {
-	public static void main(String[] args) {
-		SessionFactory sessionFactory = HibernateUtil.createSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(new TestData("Test1"));
-        session.save(new TestData("Test2"));
-      
-        session.getTransaction().commit();
- 
-        Query q = session.createQuery("From TestData ");
-                 
-        List<TestData> resultList = q.list();
-        System.out.println("num of datapoints:" + resultList.size());
-        for (TestData next : resultList) {
-            System.out.println("next data: " + next);
-        }
+	
+	public static void main(String[] args) throws Exception {
+//		SessionFactory sessionFactory = HibernateUtil.createSessionFactory();
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        session.save(new TestData("Test1"));
+//        session.save(new TestData("Test2"));
+//      
+//        session.getTransaction().commit();
+// 
+//        Query q = session.createQuery("From TestData ");
+//                 
+//        List<TestData> resultList = q.list();
+//        System.out.println("num of datapoints:" + resultList.size());
+//        for (TestData next : resultList) {
+//            System.out.println("next data: " + next);
+//        }
+//
+//        sessionFactory.close();
+		
+//////////////////////////////////////		
+//		If creating your own config.
+//		Configuration cfg = new OgmConfiguration();
+//
+//		//assuming you are using JTA in a non contained environment
+//		cfg.setProperty(Environment.TRANSACTION_STRATEGY,"org.hibernate.transaction.JTATransactionFactory");
+//		//assuming JBoss TransactionManager in standalone mode
+//		cfg.setProperty(Environment.JTA_PLATFORM,
+//		     "org.hibernate.service.jta.platform.internal.JBossStandAloneJtaPlatform");
+//
+//		//assuming the default mongo settings
+//		cfg.setProperty("hibernate.ogm.datastore.provider","org.hibernate.ogm.datastore.infinispan.impl.InfinispanDatastoreProvider");
+//
+//		//add your annotated classes
+//		cfg.addAnnotatedClass(TestData.class);
 
-        sessionFactory.close();
+		//persist the entities
+		javax.transaction.TransactionManager transactionManager = com.arjuna.ats.jta.TransactionManager.transactionManager();
+		
+		
+		EntityManager em = (EntityManager) Persistence.createEntityManagerFactory("DatabaseBenchmarking-mongodb");
+		transactionManager.begin();
+		TestData dt1 = new TestData("Test1");
+		TestData dt2 = new TestData("Test2");
+		em.persist(dt1);
+		em.persist(dt2);
+		em.flush();
+		em.close();
+		transactionManager.commit();
+		
+//		// persist the entities
+//		SessionFactory sessionFactory = HibernateUtil.createSessionFactory(cfg);
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        session.save(new TestData("Test1"));
+//        session.save(new TestData("Test2"));
+//      
+//        session.getTransaction().commit();
+// 
+//        Query q = session.createQuery("From TestData ");
+//                 
+//        List<TestData> resultList = q.list();
+//        System.out.println("num of datapoints:" + resultList.size());
+//        for (TestData next : resultList) {
+//            System.out.println("next data: " + next);
+//        }
+//
+//        sessionFactory.close();
  
     }
 }

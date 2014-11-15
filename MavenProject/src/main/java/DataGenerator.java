f26d929adc6247fdc8f1152872d59d4a7c9b58f5
Kristin Asmus <kna934@gmail.com>
2014-11-15 05:03:46 -0500

    data generator now works for strings as well

--- MavenProject/src/main/java/DataGenerator.java
+++ MavenProject/src/main/java/DataGenerator.java
@@ -4,7 +4,9 @@ import java.util.Iterator;
 import java.util.Random;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
-import org.json.*;
+import org.apache.commons.lang.RandomStringUtils;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class DataGenerator {
 	
@@ -28,6 +30,7 @@ public class DataGenerator {
 		
 		// determine distribution and sample to get distinct values
 		if (distribution.equalsIgnoreCase("delta")) {
+			// all data are the same value
 			int value = (new Random()).nextInt(max-min) + min;
 			for (int i = 0; i < count; i++) {
 				data.add(value);
@@ -85,24 +88,94 @@ public class DataGenerator {
 		String distribution = colInfo.getString("distribution");
 		if (distribution == null || !(distribution.equalsIgnoreCase("delta") || 
 				distribution.equalsIgnoreCase("uniform") || distribution.equalsIgnoreCase("normal"))) {
-			//error
+			throw new RuntimeException("distribution must be uniform, normal, or delta");
 		}
 		int distinct = colInfo.getInt("distinct");
+		if (distinct > count) distinct = count;
+		// if distinct = 0, assume they know nothing about distinctness of values
 		int max = colInfo.getInt("max");
 		int min = colInfo.getInt("min");
 		double mean = colInfo.getDouble("mean");
 		double stdv = colInfo.getDouble("stdv"); 
 		
+		HashSet<String> distinctVals = new HashSet<String>();
+		ArrayList<String> data = new ArrayList<String>();
+		Random generator = new Random();
+		
 		// determine distribution
 		if (distribution.equalsIgnoreCase("delta")) {
-			
-		} else if (distribution.equalsIgnoreCase("uniform")) {
-			
+			// all words have the same length
+			int wordLen = (new Random()).nextInt(max-min) + min;
+			if (distinct > 0) {
+				// get the distinct values to use
+				while (distinctVals.size() < distinct) {
+					distinctVals.add(RandomStringUtils.randomAlphabetic(wordLen));
+				}
+				if (distinct < count) {
+					// sample from the distinct values to get the complete data
+					ArrayList<String> distinctList = new ArrayList<String>();
+					distinctList.addAll(distinctVals);
+					for (int i = 0; i < count; i++) {
+						data.add(distinctList.get(generator.nextInt(distinct)));
+					}
+					return data.iterator();
+				} else { 
+					// distinct == count
+					return distinctVals.iterator();
+				}
+			} else {
+				// we know nothing about distinctness, so just sample
+				for (int i = 0; i < count; i++) {
+					data.add(RandomStringUtils.randomAlphabetic(wordLen));
+				}
+				return data.iterator();
+			}	
+		} else if (distribution.equalsIgnoreCase("uniform")) {			
+			if (distinct > 0) {
+				// get the distinct values to use
+				while (distinctVals.size() < distinct) {
+					int wordLen = (new Random()).nextInt(max-min) + min;
+					distinctVals.add(RandomStringUtils.randomAlphabetic(wordLen));	
+				}
+				if (distinct < count) {
+					// sample from the distinct values to get the complete data
+					ArrayList<String> distinctList = new ArrayList<String>();
+					distinctList.addAll(distinctVals);
+					for (int i = 0; i < count; i++) {
+						data.add(distinctList.get(generator.nextInt(distinct)));
+					}
+					return data.iterator();
+				} else { 
+					// distinct == count
+					return distinctVals.iterator();
+				}
+			} else {
+				// we know nothing about distinctness, so just sample
+				for (int i = 0; i < count; i++) {
+					int wordLen = (new Random()).nextInt(max-min) + min;
+					data.add(RandomStringUtils.randomAlphabetic(wordLen));
+				}
+				return data.iterator();
+			}
 		} else { //distribution: normal
-			
+			// TODO: should distinct values > count mean anything here???
+			NormalDistribution dist = new NormalDistribution(mean, stdv);
+			if (distinct == count) {
+				// all values must be distinct
+				while (distinctVals.size() < distinct) {
+					int wordLen = (int) dist.sample();
+					distinctVals.add(RandomStringUtils.randomAlphabetic(wordLen));
+				}
+				return distinctVals.iterator();
+			} else {
+				// we know nothing about distinctness, so just sample
+				for (int i = 0; i < count; i++) {
+					int wordLen = (int) dist.sample();
+					data.add(RandomStringUtils.randomAlphabetic(wordLen));
+				}
+				return data.iterator();
+			}	
 		}
-			
-		return null;
 	}
 
     private static String generateJsonData(JSONObject json) {
@@ -148,6 +221,8 @@ public class DataGenerator {
     
     // TODO: will implement this if compression is necessary
     private static byte[] compress(String output) {
+    	// string -> gzip information:
+    	// http://stackoverflow.com/questions/16351668/compression-and-decompression-of-string-data-in-java
     	return null;
     }
     
@@ -161,11 +236,11 @@ public class DataGenerator {
         		"message: {cardinality:4, fields:"+
         			"[{category:Integer,length:4,name:message_id,distribution:normal,"+
         				"distinct:0,mean:50,stdv:10,min:0,max:0},"+
-        			"]}}";
 //        			"{category:Integer,length:4,name:author_id,distribution:uniform,"+
 //        				"distinct:0,mean:0,stdv:0,min:0,max:0},"+
-//        			"{category:String,length:128,name:text,distribution:uniform,"+
-//        				"distinct:0,mean:0,stdv:0,min:0,max:0},"+
+        			"{category:String,length:128,name:text,distribution:uniform,"+
+        				"distinct:2,mean:0,stdv:0,min:3,max:6},"+
+        			"]}}";
 //        			"{category:Integer,length:4,name:pub_date,distribution:uniform,"+
 //        				"distinct:0,mean:0,stdv:0,min:0,max:0}]},"+
 //        		"user:{cardinality:0,fields:"+
@@ -179,15 +254,5 @@ public class DataGenerator {
 //        				"distinct:0,mean:0,stdv:0,min:0,max:0}]}};";
         JSONObject json = new JSONObject(sampleTables);
         System.out.println(generateJsonData(json));
-        
-        // TEST: uniformly distributed integer data generation
-//        String colString = "{category:Integer,length:128,name:rank,distribution:uniform,"+
-//				"distinct:5,mean:0,stdv:0,min:1,max:10},";
-//        JSONObject colJson = new JSONObject(colString);
-//        Iterator<Integer> it = generateIntegerData(colJson, 20);
-//        System.out.print("Int data generator produced: ");
-//        while (it.hasNext()) {
-//        	System.out.print(it.next()+", ");
-//        }
     }
 }

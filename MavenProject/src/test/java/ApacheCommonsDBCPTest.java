68a818bcdee66fe9a5b782f283f5a49d23f61bb2
shirllu <shirley.lu100@gmail.com>
2014-11-22 14:28:29 -0500

    Checking in before making api changes.

--- MavenProject/src/test/java/ApacheCommonsDBCPTest.java
+++ MavenProject/src/test/java/ApacheCommonsDBCPTest.java
@@ -14,7 +14,7 @@ public class ApacheCommonsDBCPTest {
     	for (String name: result.keySet()){
             String key =name.toString();
             String value = result.get(name).toString();  
-            System.out.println("db is: "+ key + "run time is: " + value);  
+            System.out.println("db is: "+ key + " run time is: " + value);  
     	} 
 //    	for (String name: result2.keySet()){
 //            String key =name.toString();

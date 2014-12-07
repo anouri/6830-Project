9e54097cbc5e6e4dfce3dc6411af5cbac32f29f8
Tony Tran <tmtran7393@gmail.com>
2014-12-07 17:12:31 -0500

    Add batch support in InsertData

--- MavenProject/src/main/java/InsertData.java
+++ MavenProject/src/main/java/InsertData.java
@@ -51,16 +51,123 @@ public class InsertData {
         return resultQuery;
     }
 
+    public static List<QueryArgs> QueryArgsFromJSON(String jsonData) {
+        List<QueryArgs> queryArgs = new ArrayList<QueryArgs>();
+        JSONObject jsonObject = new JSONObject(jsonData);
+        String[] tableName = null;
+        try {
+            tableName = SQLSchemaParser.getTableName();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (String table: tableName) {
+            JSONObject tableObject = jsonObject.getJSONObject(table);
+            JSONArray columnName = tableObject.getJSONArray(COLUMN_NAME); // [colName1, colName2, colName3]
+            JSONArray columnData = tableObject.getJSONArray(COLUMN_DATA);  // [ [1,2,'x'], [2,3,'y'], [3,4,'z'] ]
+            
+            // Build the INSERT query template
+            StringBuffer insertTemplate = new StringBuffer(String.format("INSERT INTO %s (",table));
+            for (int i = 0; i < columnName.length(); i++) {
+                insertTemplate.append(columnName.getString(i));
+                if (i == columnName.length()-1) {
+                    insertTemplate.append(")");
+                }
+                else {
+                    insertTemplate.append(",");
+                }
+            }
+            insertTemplate.append(" VALUES (");
+            if (columnData.length() == 0) continue;
+            int numCols = columnData.getJSONArray(0).length();
+            for (int i = 0; i < numCols; i++) {
+                insertTemplate.append("?");
+                if (i == numCols-1) {
+                    insertTemplate.append(")");
+                } else {
+                    insertTemplate.append(",");
+                }
+            }
+
+            // Build the list of values to insert with the Prepared Statement
+            Arguments[] arguments = new Arguments[columnData.length()];
+            for (int i = 0 ; i < columnData.length(); i++) {
+                JSONArray values = columnData.getJSONArray(i);    // [1,2,'x']
+                Data[] dataList = new Data[values.length()];
+                for (int j = 0; j < values.length(); j++) {
+                    Object val = values.get(j);
+                    // if (tupleDesc.getFieldType(j).getType() == Type.SupportedType.INT_TYPE) {
+                    //     dataList[j] = new Data(true, values.getInt(j));
+                    // } else {
+                    //     dataList[j] = new Data(false, values.getString(j));
+                    // }
+                    if (val instanceof String) {
+                        dataList[j] = new Data(false, (String)val);
+                    } else {
+                        dataList[j] = new Data(true, (Integer)val);
+                    }
+                }
+                arguments[i] = new Arguments(dataList);
+            }
+
+            queryArgs.add(new QueryArgs(insertTemplate.toString(), arguments));
+        }
+        if (!jsonObject.isNull("endOfData")) {
+            isEOF = true;
+        } else {
+            isEOF = false;
+        }
+        return queryArgs;
+    }
+
     public static boolean isDone() {
         return isEOF;
     }
 
     public static void main(String argsp[]) {
-        String[] tableName = new String[] {"chartevents","chartitems","follower","icustayevents","labevents","labitems","meddurations","medevents","meditems", "message","patients"};
-        QueryExecutorAll.dropTables(tableName);
+        // Test InsertStatementFromJSON
+
+        // String[] tableName = new String[] {"chartevents","chartitems","follower","icustayevents","labevents","labitems","meddurations","medevents","meditems", "message","patients"};
+        // QueryExecutorAll.dropTables(tableName);
+        // SQLSchemaParser schemaParser = new SQLSchemaParser(SQLSchemaParser.EXAMPLE_SCHEMA.trim());
+        // String createProcedure = SQLSchemaParser.getRawSchema();
+        // QueryExecutorAll.shema_creation_all(createProcedure);
+        // FastDataGenerator fdg = new FastDataGenerator(SQLSchemaParser.EXAMPLE_DISTRIBUTION);
+        // HashMap<String, Long> results = new HashMap<String, Long>();
+        // while (true) {
+        //     String distribution = fdg.generateMoreData();
+        //     if (distribution == null) {
+        //         break;
+        //     }
+        //     for (String s : InsertStatementFromJSON(distribution)) {
+        //         HashMap<String, Long> result =  QueryExecutorAll.run_all(s);
+        //         for (String key : result.keySet()) {
+        //             if (results.containsKey(key)) {
+        //                 results.put(key, results.get(key) + result.get(key));
+        //             } else {
+        //                 results.put(key, result.get(key));
+        //             }
+
+        //         }
+        //     };
+        //     for (String db: results.keySet()){
+        //         long run = results.get(db);
+        //         System.out.println("db is: "+ db + " run time is: " + run);
+        //     }
+        // }
+        // for (String db: results.keySet()){
+        //     long run = results.get(db);
+        //     System.out.println("db is: "+ db + " run time is: " + run);
+        // }
+
+        // Test QueryArgsFromJSON
+        // String[] tableName = new String[] {"chartevents","chartitems","follower","icustayevents","labevents","labitems","meddurations","medevents","meditems", "message","patients"};
+        // QueryExecutorAll.dropTables(tableName);
+        // String rawSchema = "CREATE TABLE follower (who_id int, whom_id int, primary key (who_id)); CREATE TABLE message (message_id int, text text, primary key (message_id));";
         SQLSchemaParser schemaParser = new SQLSchemaParser(SQLSchemaParser.EXAMPLE_SCHEMA.trim());
         String createProcedure = SQLSchemaParser.getRawSchema();
+        // SQLSchemaParser schemaParser = new SQLSchemaParser(rawSchema);
         QueryExecutorAll.shema_creation_all(createProcedure);
+        // String distributionJSON = "{follower: {cardinality: 3, fields: [{category: Integer, length: 4, name: who_id, distribution: autoincrement,distinct: null, mean: null, stdv: null, min: null, max: null}, { category: Integer, length: 4, name: whom_id, distribution: delta, distinct: null, mean: null, stdv: null, min: 1, max: 10}]}, message: {cardinality: 4, fields: [{category: Integer, length: 4,  name: message_id, distribution: autoincrement, distinct: null, mean: null, stdv: null, min: null, max: null}, {category: String, length: 128, name: text, distribution: uniform,distinct: 2, mean: null, stdv: null, min: 3, max: 6}]}}";
         FastDataGenerator fdg = new FastDataGenerator(SQLSchemaParser.EXAMPLE_DISTRIBUTION);
         HashMap<String, Long> results = new HashMap<String, Long>();
         while (true) {
@@ -68,8 +175,8 @@ public class InsertData {
             if (distribution == null) {
                 break;
             }
-            for (String s : InsertStatementFromJSON(distribution)) {
-                HashMap<String, Long> result =  QueryExecutorAll.run_all(s);
+            for (QueryArgs qa : QueryArgsFromJSON(distribution)) {
+                HashMap<String, Long> result =  QueryExecutorAll.bulk_run_insertion(qa);
                 for (String key : result.keySet()) {
                     if (results.containsKey(key)) {
                         results.put(key, results.get(key) + result.get(key));
@@ -79,10 +186,6 @@ public class InsertData {
 
                 }
             };
-            for (String db: results.keySet()){
-                long run = results.get(db);
-                System.out.println("db is: "+ db + " run time is: " + run);
-            }
         }
         for (String db: results.keySet()){
             long run = results.get(db);
